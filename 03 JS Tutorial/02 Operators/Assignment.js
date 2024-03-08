4eaab267079a0eaca540c986961aec5ea3b4092f
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-08 22:17:13 +0530

    JS Tutorial

---
+++ 03 JS Tutorial/02 Operators/Assignment.js
@@ -0,0 +1,30 @@
+var a = 25
+
+var b = a;
+
+console.log(b);
+
+a+=25;
+
+console.log(a);
+
+a-=25;
+
+console.log(a);
+
+a*=25;
+
+console.log(a);
+
+a/=25;
+
+console.log(a);
+
+b%=25;
+
+console.log(b);
+
+a**=2;
+
+console.log(a);
+
