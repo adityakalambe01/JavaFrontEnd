976ac3a26fc302895298c34dfe74569d90f24fb8
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-08 22:13:18 +0530

    JS Tutorial

---
+++ 03 JS Tutorial/02 Operators/Comparision.js
@@ -0,0 +1,25 @@
+var number1 = 25;
+var numberString = "25";
+var number2 = 50;
+
+console.log(number1 == numberString);
+
+console.log(number1 === numberString);
+
+console.log(number1 !== numberString);
+
+console.log(number1 !== number2);
+
+console.log(number1 !== number1);
+
+console.log(number1 === number2);
+
+console.log(number1 >= number2);
+
+console.log(number1 <= number2);
+
+console.log(number1 < number2);
+
+console.log(number1 > number2);
+
+console.log(number1 != number2);
