2a22afad5cb9299be87feab730cd61844b75e984
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-08 22:05:41 +0530

    JS Tutorial

--- 03 JS Tutorial/02 Operators/Arithmatic.js
+++ 03 JS Tutorial/02 Operators/Arithmatic.js
@@ -4,3 +4,19 @@ var b = 50;
 console.log(a+b);
 
 console.log(a-b);
+
+console.log(a/b);
+
+console.log(a%b);
+
+console.log(a*b);
+
+console.log(2**3);
+
+a++;
+
+console.log(a);
+
+b--;
+
+console.log(b);
