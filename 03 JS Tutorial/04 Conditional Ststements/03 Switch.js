df1bae11d027e9edcc3ba5320cfe43ee3bb4211c
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-09 22:52:29 +0530

    JS Tutorial

---
+++ 03 JS Tutorial/04 Conditional Ststements/03 Switch.js
@@ -0,0 +1,16 @@
+var color = "red".toLowerCase();
+
+switch (color){
+    case "red":
+        console.log("Red");
+        break;
+    case "green":
+        console.log("Green");
+        break;
+    case "blue":
+        console.log("blue");
+        break;
+    default:
+        console.log("White");
+
+}
