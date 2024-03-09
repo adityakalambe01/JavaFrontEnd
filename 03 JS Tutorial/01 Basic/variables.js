c0a9bbd5ea4b165ea712b925b930fad10dfa1b7c
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-09 13:12:08 +0530

    JS Tutorial

---
+++ 03 JS Tutorial/01 Basic/variables.js
@@ -0,0 +1,10 @@
+var stringVariable = "string";
+
+var numberVariable = 25;
+var numberVariable2 = 36.25;
+
+var booleanVariable = true
+
+process.stdout.write(` ${stringVariable} \n ${numberVariable} \n ${numberVariable2} \n ${booleanVariable}`);
+
+
