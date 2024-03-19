ad19aca7b566b90d7a41c94818659d03719420f4
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-19 12:11:17 +0530

    Angular Tutorial

--- 05 Angular/01 Data Binding/02_two_way_data_binding/src/app/app.component.ts
+++ 05 Angular/01 Data Binding/02_two_way_data_binding/src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template : `
+    <h1>Two Way Data Binding</h1>
+    <input [(ngModel)] = 'name' placeholder="Enter your name">
+    <p> Welcome {{name}}</p>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '02_two_way_data_binding';
+
+  name = '';
 }
