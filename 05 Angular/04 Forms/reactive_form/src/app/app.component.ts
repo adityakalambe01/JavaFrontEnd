d23c9b6c65cd018b84792b5302e50f22cff3e47d
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-23 23:37:51 +0530

    Angular Tutorial

--- 05 Angular/04 Forms/reactive_form/src/app/app.component.ts
+++ 05 Angular/04 Forms/reactive_form/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'reactive_form';
+export class AppComponent implements OnInit{
+  myForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+    })
+  }
+
+  submitForm() {
+    console.log("Form Submitted");
+  }
 }
