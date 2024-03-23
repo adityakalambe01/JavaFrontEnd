903885a73749f5294d9f1f929e323e414d7206d4
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-23 23:15:06 +0530

    Angular Tutorial

---
+++ 05 Angular/04 Forms/template_driven_form/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'template_driven_form';
+
+  submitForm() {
+      console.log("Template Driven Forms");
+  }
+}
